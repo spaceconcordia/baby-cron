7da9bbe1207ea98e6883f78fe3425e1d4a69ae94
spaceconcordia <spaceconcordia@mustang.(none)>
2013-01-07 15:36:17 -0500

    Refactor tests

--- tests/integration/crontab-test.cpp
+++ tests/integration/crontab-test.cpp
@@ -6,15 +6,9 @@
 #include <sys/stat.h>
 
 //position starts from 0
-void fillAndSet(char* array, int len, char value, int position) {
-    for (int i = 0; i != len; i++) {
-        if (i == position) {
-            *(array + i) = value;
-        }
-        else {
-            *(array + i) = 0;
-        }
-    }
+void clearAndSet(char* array, int len, char value, int position) {
+    memset(array, 0, len);
+    *(array + position) = value;
 }
 
 //-------------------------------------------------------------------
@@ -26,25 +20,22 @@ TEST_GROUP(CronTab) {
 
 TEST(CronTab, LoadCronTab_ValidFileOneLine_ReturnCronTabStruct) {
     CronLine* actual = (CronLine *)malloc(sizeof(CronLine));
-    char expectedDow[6]   = {0, 1, 0, 0, 0, 0};
-    char expectedMons[12] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    char expectedHrs[24]  = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0};
-    char expectedDays[31] = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0};
-    char expectedMins[60] ={ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+    char expectedDow[6]   = {0};
+    char expectedMons[12] = {0};
+    char expectedHrs[24]  = {0};
+    char expectedDays[31] = {0};
+    char expectedMins[60] = {0};
+ 
     load_crontab("crontab-oneline");
 
     actual = G.cron_files->cf_lines;
+    clearAndSet(expectedDow,   6, 1, 1);
+    clearAndSet(expectedMons, 12, 1, 0); //Months values have an offset of -1
+    clearAndSet(expectedHrs,  24, 1, 1);
+    clearAndSet(expectedDays, 31, 1, 1);
+    clearAndSet(expectedMins, 60, 1, 1);
+
     CHECK(std::equal(actual->cl_Dow,  actual->cl_Dow  + 6,  expectedDow));
     CHECK(std::equal(actual->cl_Mons, actual->cl_Mons + 12, expectedMons));
     CHECK(std::equal(actual->cl_Hrs,  actual->cl_Hrs  + 24, expectedHrs));
@@ -56,25 +47,21 @@ TEST(CronTab, LoadCronTab_ValidFileOneLine_ReturnCronTabStruct) {
 TEST(CronTab, LoadCronTab_ValidFileTwoLines_ReturnCronTabStruct) {
     CronLine* actual = (CronLine *)malloc(sizeof(CronLine));
 
-    char expectedDow[6]   = {0, 1, 0, 0, 0, 0};
-    char expectedMons[12] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    char expectedHrs[24]  = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0};
-    char expectedDays[31] = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0};
-    char expectedMins[60] ={ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+    char expectedDow[6]   = {0};
+    char expectedMons[12] = {0};
+    char expectedHrs[24]  = {0};
+    char expectedDays[31] = {0};
+    char expectedMins[60] = {0};
+ 
     load_crontab("crontab-twolines");
 
+    // First line
     actual = G.cron_files->cf_lines;
+    clearAndSet(expectedDow,   6, 1, 1);
+    clearAndSet(expectedMons, 12, 1, 0); //Months values have an offset of -1
+    clearAndSet(expectedHrs,  24, 1, 1);
+    clearAndSet(expectedDays, 31, 1, 1);
+    clearAndSet(expectedMins, 60, 1, 1);
 
     CHECK(std::equal(actual->cl_Dow,  actual->cl_Dow  + 6,  expectedDow));
     CHECK(std::equal(actual->cl_Mons, actual->cl_Mons + 12, expectedMons));
@@ -83,12 +70,13 @@ TEST(CronTab, LoadCronTab_ValidFileTwoLines_ReturnCronTabStruct) {
     CHECK(std::equal(actual->cl_Mins, actual->cl_Mins + 60, expectedMins));
     STRCMP_EQUAL(actual->cl_cmd, "command");
 
+    // Second line
     actual = (G.cron_files->cf_lines->cl_next);
-    fillAndSet(expectedDow,   6, 1, 2);
-    fillAndSet(expectedMons, 12, 1, 1);
-    fillAndSet(expectedHrs,  24, 1, 2);
-    fillAndSet(expectedDays, 31, 1, 2);
-    fillAndSet(expectedMins, 60, 1, 2);
+    clearAndSet(expectedDow,   6, 1, 2);
+    clearAndSet(expectedMons, 12, 1, 1); //Months values have an offset of -1
+    clearAndSet(expectedHrs,  24, 1, 2);
+    clearAndSet(expectedDays, 31, 1, 2);
+    clearAndSet(expectedMins, 60, 1, 2);
 
     CHECK(std::equal(actual->cl_Dow,  actual->cl_Dow  + 6,  expectedDow));
     CHECK(std::equal(actual->cl_Mons, actual->cl_Mons + 12, expectedMons));
