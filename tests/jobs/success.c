9efe106990e7e9a6a2afaa5963c2081a459eb141
Joseph Martineau <jamg85@msn.com>
2014-08-28 12:12:29 -0400

    [tests/jobs] makefile and added log in success

--- tests/jobs/success.c
+++ tests/jobs/success.c
@@ -1,3 +1,15 @@
+/******************************************************************************
+ *
+ * COMPILE : g++ success.c -o success -I../../../space-lib/include -L../../../space-lib/shakespeare/lib -L../../../space-lib/lib -lshakespeare -lcs1_utls
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include "shakespeare.h"
 int main(void) {
+    string folder   = "/home/logs/";
+    FILE *g_fp_log = Shakespeare::open_log(folder, "Baby-Cron");
+    Shakespeare::log(g_fp_log, Shakespeare::NOTICE, "Baby-Cron", "tests/jobs/success job executing");
+    fflush(g_fp_log);
+
     return 0;
 }
