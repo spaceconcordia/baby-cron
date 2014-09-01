e0e6f9f180878736e2284d719ce3ac09b7f541d2
Joseph Martineau <jamg85@msn.com>
2014-09-01 00:31:36 -0400

    [tests/jobs/success.c] change log filename

--- tests/jobs/success.c
+++ tests/jobs/success.c
@@ -7,8 +7,8 @@
 #include "shakespeare.h"
 int main(void) {
     string folder   = "/home/logs/";
-    FILE *g_fp_log = Shakespeare::open_log(folder, "Baby-Cron");
-    Shakespeare::log(g_fp_log, Shakespeare::NOTICE, "Baby-Cron", "tests/jobs/success job executing");
+    FILE *g_fp_log = Shakespeare::open_log(folder, "Baby-Cron-TestJob");
+    Shakespeare::log(g_fp_log, Shakespeare::NOTICE, "Baby-Cron-TestJob", "tests/jobs/success job executing");
     fflush(g_fp_log);
 
     return 0;
